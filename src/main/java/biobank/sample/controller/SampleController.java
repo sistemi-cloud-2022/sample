5efeef40ac2d2cca0e38f342da5e8c7b7dd05360
danilo <37438774+khalld@users.noreply.github.com>
2022-07-16 12:24:40 +0200

    Update

--- src/main/java/biobank/sample/controller/SampleController.java
+++ src/main/java/biobank/sample/controller/SampleController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
